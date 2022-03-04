7752dd59fdc4bc477bdadb2bf79b138ca4e97a78
Mathieu Bissonnette <mathieu.bissonnette2@mail.mcgill.ca>
2022-03-04 13:38:18 -0500

    Add the step definition for owner creation

--- src/test/java/acceptance/steps/stepDefinition.java
+++ src/test/java/acceptance/steps/stepDefinition.java
@@ -1,5 +1,14 @@
 package acceptance.steps;
 
-public class stepDefinition {
+import io.cucumber.java.en.Given;
+import io.cucumber.datatable.DataTable;
 
+import java.util.List;
+import java.util.Map;
+
+public class stepDefinition {
+	@Given("the following owners exist in the system:")
+	public void theFollowingOwnersExistInTheSystem(DataTable ownerTable) {
+		List<Map<String, String>> owners = ownerTable.asMaps(String.class, String.class);
+	}
 }
