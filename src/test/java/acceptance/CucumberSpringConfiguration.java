bf93fa611bb143611e27d0e46602d1945f3d6fa4
Mathieu Bissonnette <mathieu.bissonnette2@mail.mcgill.ca>
2022-03-09 18:32:09 -0500

    Add annotation to restart app for each scenario
    Add DirtiesContext to CucumberSpringConfiguration to clean the context
    between each scenario of the acceptance tests.

--- src/test/java/acceptance/CucumberSpringConfiguration.java
+++ src/test/java/acceptance/CucumberSpringConfiguration.java
@@ -3,10 +3,12 @@ package acceptance;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.samples.petclinic.PetClinicApplication;
+import org.springframework.test.annotation.DirtiesContext;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = { PetClinicApplication.class, CucumberRunnerTest.class },
 		webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class CucumberSpringConfiguration {
 
 }
