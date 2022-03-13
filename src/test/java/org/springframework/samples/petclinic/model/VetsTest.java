ef95e54b06f4dd0d47810ee966321ae8a38cd0f1
omar-abdelaziz-git <omar.abdelaziz@mail.mcgill.ca>
2022-03-13 00:18:50 -0500

    Unit Test for Model Done

--- src/test/java/org/springframework/samples/petclinic/model/VetsTest.java
+++ src/test/java/org/springframework/samples/petclinic/model/VetsTest.java
@@ -6,11 +6,20 @@ import org.junit.jupiter.api.*;
 import java.time.LocalDate;
 import java.util.*;
 public class VetsTest {
+    static Vets vets_list=new Vets();
+    static List<Vet> list=new ArrayList <Vet> ();
     @Test
     @Order(1)
     void getVetListTest(){
-        Vets vets_list=new Vets();
-        Vet vet_1=new Vet();
-        
+        assertEquals(list, vets_list.getVetList());
     }
+
+
+    @AfterAll
+   static void teardown() {
+    vets_list=null;
+    list=null;
+    
+   
+}
 }
