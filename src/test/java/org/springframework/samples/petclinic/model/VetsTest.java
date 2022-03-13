a6133163e9b5ca315827948050e73d933304d9e1
omar-abdelaziz-git <omar.abdelaziz@mail.mcgill.ca>
2022-03-12 23:56:15 -0500

    All Model Unit Tests

---
+++ src/test/java/org/springframework/samples/petclinic/model/VetsTest.java
@@ -0,0 +1,16 @@
+package org.springframework.samples.petclinic.model;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDate;
+import java.util.*;
+public class VetsTest {
+    @Test
+    @Order(1)
+    void getVetListTest(){
+        Vets vets_list=new Vets();
+        Vet vet_1=new Vet();
+        
+    }
+}
